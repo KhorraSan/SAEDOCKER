cae6e97d94b118336ee80e15f099b9b188d400d9
Assa750ssin <114162158+Assa750ssin@users.noreply.github.com>
2025-01-15 12:38:49 +0100

    Add files via upload

---
+++ Codefinal/config/connexion.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$usernameSQL = "root";
+$passwordSQL = "";
+$dbname = "docker";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $usernameSQL, $passwordSQL);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch(PDOException $e) {
+    echo "Error creating tables: " . $e->getMessage();
+}
+
+?>
