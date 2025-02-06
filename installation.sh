b62f6cbbe38abfa811691e6acaeb010ed20493cc
Assa750ssin <114162158+Assa750ssin@users.noreply.github.com>
2025-02-07 00:39:58 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -49,12 +49,12 @@ sudo mkdir -p $SRC_DIR
 
 
 echo "✍️ Création du fichier docker-compose.yml..."
-sudo bash -c "cat <<EOF > $CONFIG_DIR/docker-compose.yml
-version: '3.9'
+sudo bash -c 'cat <<EOF > $CONFIG_DIR/docker-compose.yml
+version: "3.9"
 
 services:
   www:
-    build: .
+    build: ./src
     ports:
       - "8000:80"
     volumes:
@@ -124,7 +124,7 @@ services:
   my-keycloak:
     image: quay.io/keycloak/keycloak:24.0
     environment:
-      KC_HOSTNAME: $IP_ADDRESS
+      KC_HOSTNAME: '"$IP_ADDRESS"'
       KC_HOSTNAME_PORT: 7080
       KC_HOSTNAME_STRICT_BACKCHANNEL: "true"
       KEYCLOAK_ADMIN: admin
@@ -132,7 +132,7 @@ services:
       KC_HEALTH_ENABLED: "true"
       KC_LOG_LEVEL: info
     healthcheck:
-      test: ["CMD", "curl", "-f", "http://$IP_ADDRESS:7080/health/ready"]
+      test: ["CMD", "curl", "-f", "http://'"$IP_ADDRESS"':7080/health/ready"]
       interval: 15s
       timeout: 2s
       retries: 15
@@ -155,7 +155,7 @@ volumes:
 networks:
   backend:
     driver: bridge
-EOF
+EOF'
 
 echo "✅ Fichier docker-compose.yml créé avec l'IP : $IP_ADDRESS"
 
